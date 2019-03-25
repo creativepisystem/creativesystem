4987ec31bc01cb1a3c8de2af5f658fdbb726a594
Felipe <felipe.goulart1999@gmail.com>
2019-03-25 13:04:03 -0300

    Enum

---
+++ C#/Truckleer/Truckleer.Api/src/Enum/FormsEnum.cs
@@ -0,0 +1,12 @@
+using System.Flags
+
+namespace Truckleer.Api.Enum
+{
+    enum Generos
+    {
+        backgorund("#2F4F4F"),
+        components("#0000CD"),
+        font(new Font(FontFamily.GenericSansSerif,
+            12.0F, FontStyle.Bold))
+    };
+}
